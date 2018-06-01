ce81c8348a9e9be6b9bca6f15836b3b04a7e8166
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-06-01 14:41:57 -0400

    Completes HashSet

--- RUBY/lib/p03_hash_set.rb
+++ RUBY/lib/p03_hash_set.rb
@@ -7,15 +7,29 @@ class HashSet
   end
 
   def insert(key)
+    unless include?(key)    
+      @store[index(key)] << key 
+      @count += 1
+      resize! if count == num_buckets
+    end
   end
 
   def include?(key)
+    @store[index(key)].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      @store[index(key)].delete(key)
+      @count -= 1
+    end 
   end
 
   private
+  
+  def index(key)
+    index = key.hash % num_buckets
+  end
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
@@ -26,5 +40,12 @@ class HashSet
   end
 
   def resize!
+    array = Array.new(num_buckets * 2) { Array.new }
+    
+    @store.flatten.each do |el|
+      resize_idx = el.hash % array.length
+      array[resize_idx] << el
+    end 
+    @store = array
   end
 end
