d3ecbc5cab1841ec000e19ed8a25430b85335426
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-06-01 14:28:48 -0400

    Complete hash functions

--- RUBY/lib/p02_hashing.rb
+++ RUBY/lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    acc = 1
+    self.each_with_index do |el, idx|
+      if idx.even?
+        acc *= el
+      else 
+        acc += el
+      end 
+    end 
+    acc 
   end
 end
 
 class String
   def hash
+    arr = self.unpack('C*')
+    arr.hash
   end
 end
 
@@ -16,6 +27,11 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    str = self.to_a.flatten.map(&:to_s)
+    hash_value = 1
+    str.each do |let|
+      hash_value *= let.ord
+    end
+    hash_value
   end
 end
